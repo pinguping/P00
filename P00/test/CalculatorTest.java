864724ce0dde38e869c5a2d4326d29598e558080
21024120 <21024120@DESKTOP-AUO8UAL>
2022-05-30 13:03:10 +0800

    first push to P00 repository in GitHub

--- P00/test/CalculatorTest.java
+++ P00/test/CalculatorTest.java
@@ -25,8 +25,52 @@ public class CalculatorTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAdd() {
+		//fail("Not yet implemented");
+		int a = 1234;
+		int b = 8765;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.add(a, b);
+		
+		int expected = 9999;
+		assertEquals (expected,actual);
+	}
+	
+	@Test
+	public void testSubtract() {
+		int a = 9876;
+		int b = 4321;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.subtract(a, b);
+		
+		int expected = 5555;
+		assertEquals(actual, expected);
+	}
+	
+	@Test
+	public void testMultiple() {
+		int a = 101;
+		int b = 66;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.multiple(a, b);
+		
+		int expected = 6666;
+		assertEquals(actual, expected);
+	}
+	
+	@Test
+	public final void testDivide() {
+		int a = 23331;
+		int b = 7;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.divide(a, b);
+		
+		int expected = 3333;
+		assertEquals(actual, expected);
 	}
 
 }
